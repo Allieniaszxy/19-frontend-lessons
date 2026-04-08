e8be8ac46c9e923077f791783488da7e5922d474
Allieniaszxy <alakaalamin40@gmail.com>
2026-04-08 12:18:53 +0100

    form-validator

--- form-validator/script.js
+++ form-validator/script.js
@@ -34,5 +34,78 @@ form.addEventListener("click", function (e) {
 
   if (isFormValid) {
     alert("Registration successful");
+    form.reset();
+    document.querySelectorAll(".form-group").forEach((group) => {
+      group.className = "form-group";
+    });
   }
 });
+
+function checkPasswordMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, "Passwords do not match");
+    return false;
+  }
+  return true;
+}
+
+function checkEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (emailRegex.test(email.value.trim())) {
+    showSuccess(email);
+    return true;
+  } else {
+    showError(email, "Email is not valid");
+    return false;
+  }
+}
+
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${formatFieldName(input)} must be at least ${min} characters.`,
+    );
+    return false;
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${formatFieldName(input)} must be less than ${max} characters.`,
+    );
+    return false;
+  } else {
+    showSuccess(input);
+    return true;
+  }
+}
+
+function checkRequired(inputArray) {
+  let isValid = true;
+
+  inputArray.forEach((input) => {
+    if (input.value.trim() === "") {
+      showError(input, `${formatFieldName(input)} is required`);
+      return false;
+    } else {
+      showSuccess(input);
+    }
+  });
+
+  return isValid;
+}
+
+function formatFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+function showError(input, message) {
+  const formGroup = input.parentElement;
+  formGroup.className = "form-group error";
+  const small = formGroup.querySelector("small");
+  small.innerText = message;
+}
+
+function showSuccess(input) {
+  const formGroup = input.parentElement;
+  formGroup.className = "form-group success";
+}
